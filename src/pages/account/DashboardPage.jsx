c46a09db0d97eb999a37fcfa6d831fdbabef2fb7
Zohaib <zohaib.codemaster@gmail.com>
2026-08-06 17:07:26 +0500

    connect transaciton history section on dashboard and recent activity tab with backend

--- src/pages/account/DashboardPage.jsx
+++ src/pages/account/DashboardPage.jsx
@@ -10,7 +10,6 @@ import {
   mockAccountHolder,
   mockLatestBalance,
   mockUserProfile,
-  mockTransactions,
 } from '../../features/dashboard/mockData';
 
 export const DashboardPage = ({ onNavigate }) => {
@@ -73,7 +72,6 @@ export const DashboardPage = ({ onNavigate }) => {
 
         {/* Recent Activity Table with View All Button */}
         <TransactionHistory
-          transactions={mockTransactions}
           limit={5}
           showViewAll={true}
           hideSummaryKpi={true}
