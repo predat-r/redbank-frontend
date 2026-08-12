de8526a02b27d7bda0d7835b002c87289ef3f557
Ahmad Tariq <dev.ahmadtariq@gmail.com>
2026-08-12 15:10:58 +0500

    feat(e2e): write chatbot test cases

--- e2e/support/hooks.js
+++ e2e/support/hooks.js
@@ -5,6 +5,7 @@ import { LoginPage } from './pages/LoginPage.js';
 import { AdminRegistrationsPage } from './pages/AdminRegistrationsPage.js';
 import { RegistrationPage } from './pages/RegistrationPage.js';
 import { ProfilePage } from './pages/ProfilePage.js';
+import { ChatPage } from './pages/ChatPage.js';
 
 setDefaultTimeout(60000);
 
@@ -34,6 +35,7 @@ Before(async function () {
     registration: new RegistrationPage(this.driver, this.baseUrl),
     adminRegistrations: new AdminRegistrationsPage(this.driver, this.baseUrl),
     profile: new ProfilePage(this.driver, this.baseUrl),
+    chat: new ChatPage(this.driver, this.baseUrl),
   };
 });
 
